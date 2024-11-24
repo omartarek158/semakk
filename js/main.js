4d93301c35433ccaadb0774678a28b2b74684065
omartarek158 <doctorotmss@gmail.com>
2024-11-24 20:35:15 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let btn = document.getElementById('btn')
+
+window.onscroll = function(){
+    if(scrollY >= 400)
+    {
+        btn.style.display = 'block';
+    }else{
+        btn.style.display = 'none';
+    }
+}
+
+btn.onclick = function(){
+    scroll({
+        left:0,
+        top:0,
+        behavior:'smooth'
+    })
+}
+
+
+// let read = document.getElementById('read');
+
+// read.onclick = function(){
+//     document.h6.style.display = 'none'
+// }
